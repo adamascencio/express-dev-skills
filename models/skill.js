fda1010441513a06994849444669f1e2f143a97f
adamascencio <ascencioadam@gmail.com>
2022-07-13 15:48:13 -0700

    add mvc starter files

---
+++ models/skill.js
@@ -0,0 +1,6 @@
+const skills = [
+  {lang: 'HTML/CSS', difficulty: 'easy'},
+  {lang: 'JS', difficulty: 'intermediate'},
+  {lang: 'Node', difficulty: 'hard'},
+  {lang: 'Express', difficulty: 'hard'}
+];
